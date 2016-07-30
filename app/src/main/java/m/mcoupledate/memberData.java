12a4dda658101213e2b27ef29ceb0f933473eb98
bbkk5401 <bbkk5401@gmail.com>
2016-07-30 22:46:49 +0800

    Hui:MemberData頁面1

--- app/src/main/java/m/mcoupledate/memberData.java
+++ app/src/main/java/m/mcoupledate/memberData.java
@@ -13,9 +13,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MemberData extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    //下拉式選單
+    private Spinner b_year;
+    ArrayList<Integer>byear = new ArrayList<>();
+    private Spinner b_month;
+    ArrayList<Integer>bmonth = new ArrayList<>();
+    private Spinner b_day;
+    ArrayList<Integer>bday = new ArrayList<>();
+    private Spinner r_year;
+    ArrayList<Integer>ryear = new ArrayList<>();
+    private Spinner r_month;
+    ArrayList<Integer>rmonth = new ArrayList<>();
+    private Spinner r_day;
+    ArrayList<Integer>rday = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +63,51 @@ public class MemberData extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //下拉式選單
+        b_year = (Spinner)findViewById(R.id.b_year);
+        b_month = (Spinner)findViewById(R.id.b_month);
+        b_day = (Spinner)findViewById(R.id.b_day);
+        r_year = (Spinner)findViewById(R.id.r_year);
+        r_month = (Spinner)findViewById(R.id.r_month);
+        r_day = (Spinner)findViewById(R.id.r_day);
+        DateFormat stringFormatter = new SimpleDateFormat("yyyy-MM-dd");//要轉成String的
+        Date now = Calendar.getInstance().getTime();//取得現在時間
+        String today = stringFormatter.format(now);//將取得的時間轉成String
+        int a = Integer.valueOf(today.substring(0, 4));
+        for(int i = a ; i >= 1900 ; i--){
+            byear.add(i);
+            ryear.add(i);
+        }
+        for(int i = 1 ; i <= 12 ; i++){
+            bmonth.add(i);
+            rmonth.add(i);
+        }
+        for(int i = 1 ; i <= 31 ; i++){
+            bday.add(i);
+            rday.add(i);
+        }
+        //建立ArrayAdapter
+        ArrayAdapter<Integer> adapter_b_year = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, byear);
+        ArrayAdapter<Integer> adapter_b_month = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, bmonth);
+        ArrayAdapter<Integer> adapter_b_day = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, bday);
+        ArrayAdapter<Integer> adapter_r_year = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, ryear);
+        ArrayAdapter<Integer> adapter_r_month = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, rmonth);
+        ArrayAdapter<Integer> adapter_r_day = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, rday);
+        //ArrayAdapter顯示格式
+        adapter_b_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_b_month.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_b_day.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_r_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_r_month.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter_r_day.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //設定Spinner的資料來源
+        b_year.setAdapter(adapter_b_year);
+        b_month.setAdapter(adapter_b_month);
+        b_day.setAdapter(adapter_b_day);
+        r_year.setAdapter(adapter_r_year);
+        r_month.setAdapter(adapter_r_month);
+        r_day.setAdapter(adapter_r_day);
     }
 
     @Override
